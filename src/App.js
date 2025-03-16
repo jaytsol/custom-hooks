c77c64882437b1c9187a1e3632271cdd75f84896
jaytsol <jaytsol@naver.com>
2025-03-16 16:36:08 +0900

    added craco

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useTitle } from "./hooks/useFadeIn/useTitle";
+import { useTitle } from "@/hooks/useFadeIn/useTitle";
 
 function App() {
   const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"));
+  setTimeout(() => titleUpdater("Home"), 500);
   return (
     <div>
       <div>Hi</div>
