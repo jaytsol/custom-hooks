df41869a21a4f1650654e2011cd68d765e1597d5
jaytsol <jaytsol@naver.com>
2025-03-16 16:33:25 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTitle } from "./hooks/useFadeIn/useTitle";
 
 function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Hi</div>
     </div>
   );
 }
